ebc4e6669ccaa0a539bfce8aca386ce29ac70f58
maran8394 <maran8394@gmail.com>
2023-02-03 12:09:12 +0530

    matching template added

--- learn.py
+++ learn.py
@@ -91,7 +91,7 @@ class OpenCVTut:
                 break
         cap.release()
 
-    def findCorners(self):
+    def findCorners(self):  
         img = cv2.imread("assets/chessboard1.png")
         img = cv2.resize(img, (0,0), fx=0.5,fy=0.5)
         gray1 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -115,7 +115,33 @@ class OpenCVTut:
         d = cv2.waitKey(0)
         cv2.destroyAllWindows()
         
+    def mathingTemplate(self):
+        base_img = cv2.imread("assets/soccer_practice.jpg",0)
+        template = cv2.imread("assets/shoe.png",0)
+        
+        h,w = template.shape
+        methods = [
+            cv2.TM_CCOEFF,
+            cv2.TM_CCOEFF_NORMED,
+            cv2.TM_CCORR,
+            cv2.TM_CCORR_NORMED,
+            cv2.TM_SQDIFF,
+            cv2.TM_SQDIFF_NORMED,
+        ]
+        for method in methods:
+            img2 = base_img.copy()
+            result = cv2.matchTemplate(img2,template,method)
+            min_val,max_val, min_loc,max_loc = cv2.minMaxLoc(result)
+            
+            if method in [cv2.TM_SQDIFF,cv2.TM_SQDIFF_NORMED,]:
+                location = min_loc
+            else:
+                location = max_loc
 
-
+            bottom_right = (location[0]+w, location[1]+h)
+            cv2.rectangle(img2,location,bottom_right,(0,255,0),3)
+            cv2.imshow("df", img2)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 o = OpenCVTut()
-o.findCorners()
+o.mathingTemplate()
